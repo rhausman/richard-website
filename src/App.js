8f5268c2f18e5f8881e56e830b77d2aedc97ea6c
Richard Hausman <richard.hausman@thenotcompany.com>
2020-08-27 19:43:26 -0400

    Implemented Parallax effect, got rid of Background-Slider class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-//import logo from './Assets/logo.svg';
-import './Styles/App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Router, Route, Link } from "react-router-dom";
+import { createBrowserHistory as createHistory } from "history";
 //import custom components
+
 //import RNavbar from './Components/RNavbar'
 import HomePage from './HomePage';
 
+//assets
+//import logo from './Assets/logo.svg';
+import './Styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//setup
+const history = createHistory();
+
+//const homepage = <HomePage parallaxImage={parallaxImage} />
 function App() {
   return (
-    <HomePage />
+    <Router history={history}>
+      <Route path="/" exact component={HomePage} />
+      
+    </Router>
   );
 }
 
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,40 +1,67 @@
 import React, { useState, useEffect } from "react";
-//import { Parallax, Background } from "react-parallax";
-//import "./Styles/HomePage.css";
+import { Parallax, Background } from "react-parallax";
+import "./Styles/HomePage.css";
 import logo from './Assets/logo.svg';
 import './Styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import custom components
 import RNavbar from './Components/RNavbar'
 
-function HomePage() {
+//assets
+import parallaxImage from "./Assets/Alp.JPG"
+
+function HomePage(props) {
+    const {paralaxImage} = props
     const [initialized, setInitialized] = useState(false);
     const [images, setImages] = useState([]);
     const [layers, setLayers] = useState([]);
     return (
-      <div className="Background-Slide">
-        <div className="App">
-          
-          <RNavbar className="RNavbarTop" expand="lg">
-            Welcome to my Website
-          </RNavbar>
-  
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      </div>
+        <Parallax
+            blur={5}
+            bgImage={parallaxImage}
+            bgImageAlt="Parallax Image"
+            strength={600}
+            renderLayer={percentage => (
+            <div>
+                <div
+                style={{
+                    position: "absolute",
+                    background: `white`,
+                    left: "50%",
+                    top: "50%",
+                    borderRadius: "50%",
+                    transform: "translate(-50%,-50%)",
+                    width: percentage * 500,
+                    height: percentage * 500,
+                }}
+                />
+            </div>
+            )}
+        >
+        
+            <div className="App">
+            
+            <RNavbar className="RNavbarTop" expand="lg">
+                Welcome to my Website
+            </RNavbar>
+    
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+            </header>
+            </div>
+            
+        </Parallax>
     );
   }
 
