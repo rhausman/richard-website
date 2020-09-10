2384f1c874204f7b54cc3c7b22f316110c947e64
Richard Hausman <richard.hausman@thenotcompany.com>
2020-09-10 14:50:54 -0400

    Fixed issue with ParallaxBar width on initial page render

--- src/Components/ParallaxBar.js
+++ src/Components/ParallaxBar.js
@@ -6,15 +6,15 @@ function ParallaxBar({percentage, topPercent}){
 
     //console.log("PERCENT: ",toLog,": ", percentage-(parseInt(topPercent)/100) )
     //console.log("WIDTH: "+(percentage- ((parseInt(topPercent)/100 ))+0.01 ) * 3000)
-    
+    //console.log("PERCENTAGE: ", percentage)
     const styles = {
         position: "absolute",
         background:`red`,
         opacity:percentage,
         left: "50%",
         top: "50%",
-        width: percentage<=0?5:(percentage- ((parseInt(topPercent)/100 ))+0.01 ) * 3000 ,
-        height: percentage<=0?5:(percentage+0.05) * 20,
+        width: percentage<=0||percentage==1.0?5:(percentage- ((parseInt(topPercent)/100 ))+0.01 ) * 3000 ,
+        height: percentage<=0||percentage==1.0?5:(percentage+0.05) * 20,
         transform: "translate(-50%,-50%)"
     }
     //console.log(percentage)
